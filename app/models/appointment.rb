368cbe661949d612145c0d2665738b00b9f7645f
IrynaBk <iraboyko098@gmail.com>
2023-05-28 13:41:35 +0300

    fix bugs 2.0

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,14 +3,14 @@ class Appointment < ApplicationRecord
   belongs_to :patient
   has_one :recommendation, dependent: :destroy
 
-  validates :verify_doctor, on: :create # verify if doctor's open appointments<= 10 
+  validate :verify_doctor, on: :create # verify if doctor's open appointments<= 10 
   
   enum status: { open: 0, closed: 1 }
 
   def verify_doctor
     open_appointments_count = doctor.appointments.where(status: 0).count
     if open_appointments_count >= 10
-      errors.add("Doctor is not available.")
+      errors.add(:base, "Doctor is not available.")
     end
   end
 end
